1c75025fa66ff1424cd234c699211734f26124e3
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-07-15 20:39:45 +0800

    cucumber now starts the web application when running

--- src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/LoginSteps.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/LoginSteps.java
@@ -1,7 +1,19 @@
 package com.odde.bbuddy.acceptancetest.steps;
 
+import com.odde.bbuddy.Application;
 import cucumber.api.java8.En;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
+@WebAppConfiguration
+@IntegrationTest
 public class LoginSteps implements En {
 
     {
