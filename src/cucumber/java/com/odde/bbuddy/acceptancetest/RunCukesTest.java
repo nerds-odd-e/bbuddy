9763e6eae00959659e5b6e192bb2b275d5613af6
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-07-15 16:30:49 +0800

    use cucumber gradle plugin to run cucumber

--- src/cucumber/java/com/odde/bbuddy/acceptancetest/RunCukesTest.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = {"com.odde.bbuddy.acceptancetest.steps"}, format = {
+@CucumberOptions(features = "src/cucumber/resources/features", glue = {"com.odde.bbuddy.acceptancetest.steps"}, format = {
         "pretty", "html:target/cucumber", "json:target/cucumber-report.json"})
 public class RunCukesTest {
 }
--- src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/LoginSteps.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/LoginSteps.java
@@ -1,24 +1,17 @@
 package com.odde.bbuddy.acceptancetest.steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 
 public class LoginSteps implements En {
 
     {
         Given("^there is a user named \"([^\"]*)\" and password is \"([^\"]*)\"$", (String arg1, String arg2) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
         });
 
         When("^login with user name \"([^\"]*)\" and password \"([^\"]*)\"$", (String arg1, String arg2) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
         });
 
         Then("^login successfully$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
         });
     }
 }
