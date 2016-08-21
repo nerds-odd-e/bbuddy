b9f599b57f46be08b25d3229ef96f9827abece9c
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-08-21 21:58:00 +0800

    add transaction: description, date and amount can now be input

--- src/cucumber/java/com/odde/bbuddy/acceptancetest/pages/AddTransactionPage.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/pages/AddTransactionPage.java
@@ -1,6 +1,7 @@
 package com.odde.bbuddy.acceptancetest.pages;
 
 import com.odde.bbuddy.acceptancetest.driver.UiDriver;
+import com.odde.bbuddy.acceptancetest.driver.UiElement;
 import com.odde.bbuddy.acceptancetest.driver.UiSelect;
 import com.odde.bbuddy.acceptancetest.steps.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,11 @@ public class AddTransactionPage {
         driver.navigateTo("/add_transaction");
         UiSelect type = driver.findSelectByName("type");
         type.selectByVisibleText(transaction.getType());
+        UiElement description = driver.findElementByName("description");
+        description.sendKeys(transaction.getDescription());
+        UiElement date = driver.findElementByName("date");
+        date.sendKeys(transaction.getDate());
+        UiElement amount = driver.findElementByName("amount");
+        amount.sendKeys(transaction.getAmount());
     }
 }
--- src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/Transaction.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/Transaction.java
@@ -8,6 +8,6 @@ public class Transaction {
     private String type;
     private String description;
     private String date;
-    private Integer amount;
+    private String amount;
 
 }
