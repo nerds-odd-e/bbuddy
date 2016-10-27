c40c729191ebe7fb0c248a2cad11f38ffbd524b6
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-10-27 21:52:00 +0800

    check is the same instance as original

--- src/test/java/com/odde/bbuddy/common/view/ModelAndViewCombinerTest.java
+++ src/test/java/com/odde/bbuddy/common/view/ModelAndViewCombinerTest.java
@@ -11,15 +11,14 @@ public class ModelAndViewCombinerTest {
 
     @Test
     public void merge_model_map_entry() {
-        ModelAndView afterCombined = combinerWithOriginal().combineWith(
+        ModelAndView original = new ModelAndView();
+
+        ModelAndView afterCombined = new ModelAndViewCombiner(original).combineWith(
                 toBeCombined("key", "value"), toBeCombined("anotherKey", "anotherValue"));
 
+        assertThat(afterCombined).isSameAs(original);
         assertThat(afterCombined.getModelMap()).containsExactly(
                 new SimpleEntry<>("key", "value"), new SimpleEntry<>("anotherKey", "anotherValue"));
-    }
-
-    private ModelAndViewCombiner combinerWithOriginal() {
-        return new ModelAndViewCombiner(new ModelAndView());
     }
 
     private ModelAndView toBeCombined(String key, String value) {
