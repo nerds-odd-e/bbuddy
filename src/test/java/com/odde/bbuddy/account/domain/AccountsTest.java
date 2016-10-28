7b56bcec34189aa79139c2c770ac7c032fd984ff
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-10-28 15:01:06 +0800

    refactoring: accounts tests verify run instead of return success or failed

--- src/test/java/com/odde/bbuddy/account/domain/AccountsTest.java
+++ src/test/java/com/odde/bbuddy/account/domain/AccountsTest.java
@@ -3,9 +3,6 @@ package com.odde.bbuddy.account.domain;
 import com.odde.bbuddy.account.repo.AccountRepo;
 import org.junit.Test;
 
-import static com.odde.bbuddy.common.callback.PostActionsFactory.failed;
-import static com.odde.bbuddy.common.callback.PostActionsFactory.success;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
@@ -13,11 +10,13 @@ public class AccountsTest {
 
     AccountRepo mockAccountRepo = mock(AccountRepo.class);
     Accounts accounts = new Accounts(mockAccountRepo);
+    Account account = new Account();
+
+    Runnable afterSuccess = mock(Runnable.class);
+    Runnable afterFailed = mock(Runnable.class);
 
     @Test
     public void should_save_account() {
-        Account account = new Account();
-
         accounts.add(account);
 
         verify(mockAccountRepo).save(account);
@@ -25,14 +24,24 @@ public class AccountsTest {
     
     @Test
     public void should_return_success_when_save_with_no_exception() {
-        assertThat(accounts.add(new Account())).isInstanceOf(success().getClass());
+        accounts.add(account)
+                .success(afterSuccess)
+                .failed(afterFailed);
+
+        verify(afterSuccess).run();
+        verify(afterFailed, never()).run();
     }
 
     @Test
     public void should_return_failed_when_save_with_exception() {
         given_account_repo_save_will_failed();
 
-        assertThat(accounts.add(new Account())).isInstanceOf(failed().getClass());
+        accounts.add(account)
+                .success(afterSuccess)
+                .failed(afterFailed);
+
+        verify(afterSuccess, never()).run();
+        verify(afterFailed).run();
     }
 
     private void given_account_repo_save_will_failed() {
