1019c2a2e489574773317b9603b9507704c0cb2a
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-11-01 22:37:09 +0800

    past validator: implemented

--- src/main/java/com/odde/bbuddy/common/entity/PastValidatorForLocalDate.java
+++ src/main/java/com/odde/bbuddy/common/entity/PastValidatorForLocalDate.java
@@ -2,9 +2,22 @@ package com.odde.bbuddy.common.entity;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.time.Clock;
 import java.time.LocalDate;
 
+import static java.time.LocalDate.now;
+
 public class PastValidatorForLocalDate implements ConstraintValidator<Past, LocalDate> {
+    private final Clock clock;
+
+    public PastValidatorForLocalDate() {
+        this(Clock.systemDefaultZone());
+    }
+
+    public PastValidatorForLocalDate(Clock clock) {
+        this.clock = clock;
+    }
+
     @Override
     public void initialize(Past constraintAnnotation) {
 
@@ -12,6 +25,9 @@ public class PastValidatorForLocalDate implements ConstraintValidator<Past, Loca
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        return true;
+        if (value == null)
+            return true;
+
+        return value.isBefore(now(clock)) || value.isEqual(now(clock));
     }
 }
---
+++ src/test/java/com/odde/bbuddy/common/entity/PastValidatorForLocalDateTest.java
@@ -0,0 +1,52 @@
+package com.odde.bbuddy.common.entity;
+
+import org.junit.Test;
+
+import javax.validation.ConstraintValidatorContext;
+import java.time.Clock;
+import java.time.LocalDate;
+
+import static com.odde.bbuddy.common.Formats.parseDay;
+import static java.time.ZoneId.systemDefault;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class PastValidatorForLocalDateTest {
+
+    LocalDate today = parseDay("2016-07-01");
+    PastValidatorForLocalDate validator = new PastValidatorForLocalDate(stubClock(today));
+    ConstraintValidatorContext stubConstraintValidatorContext = mock(ConstraintValidatorContext.class);
+
+    @Test
+    public void future_local_date_return_false() {
+        LocalDate future = parseDay("2016-07-02");
+
+        assertThat(valid(future)).isEqualTo(false);
+    }
+
+    @Test
+    public void past_local_date_return_true() {
+        LocalDate past = parseDay("2016-06-30");
+
+        assertThat(valid(past)).isEqualTo(true);
+    }
+    
+    @Test
+    public void today_return_true() {
+        assertThat(valid(today)).isEqualTo(true);
+    }
+    
+    @Test
+    public void null_return_true() {
+        assertThat(valid(null)).isEqualTo(true);
+    }
+
+    private Clock stubClock(LocalDate today) {
+        return Clock.fixed(today.atStartOfDay(systemDefault()).toInstant(), systemDefault());
+    }
+
+    private boolean valid(LocalDate dateToBeValidated) {
+        return validator.isValid(dateToBeValidated, stubConstraintValidatorContext);
+    }
+
+}
