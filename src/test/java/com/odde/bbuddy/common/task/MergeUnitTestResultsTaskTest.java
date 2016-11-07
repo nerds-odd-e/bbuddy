d46bf9b9cc8e1d6cbf654ff54bc91730ca0ce6aa
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-11-07 23:51:12 +0800

    unit tests count is now correctly shown in sonar

--- src/test/java/com/odde/bbuddy/common/task/MergeUnitTestResultsTaskTest.java
+++ src/test/java/com/odde/bbuddy/common/task/MergeUnitTestResultsTaskTest.java
@@ -6,14 +6,17 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
 import static java.io.File.separator;
 import static java.lang.String.format;
-import static org.apache.commons.io.FileUtils.copyFileToDirectory;
-import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static javax.xml.parsers.DocumentBuilderFactory.newInstance;
+import static org.apache.commons.io.FileUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
 import static org.junit.Assert.assertEquals;
@@ -25,8 +28,15 @@ public class MergeUnitTestResultsTaskTest {
     private final String taskName = "mergeUnitTestResults";
     private final String testResultFileNamePattern = "TEST-com.odde.bbuddy.transaction.controller.%s.xml";
     private BuildResult result;
-    private String targetTestResultsFolder;
     private String sourceTestResultsFolder;
+    private String defaultXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<testsuite name=\"com.odde.bbuddy.RequireMergeTest$SubClass\" tests=\"1\" skipped=\"0\" failures=\"0\" errors=\"0\" timestamp=\"2016-11-06T10:32:48\" hostname=\"Josephs-MBP-2\" time=\"0.041\">\n" +
+            "  <properties/>\n" +
+            "  <testcase name=\"should_not_add_account\" classname=\"com.odde.bbuddy.RequireMergeTest$SubClass\" time=\"0.041\"/>\n" +
+            "  <system-out><![CDATA[]]></system-out>\n" +
+            "  <system-err><![CDATA[]]></system-err>\n" +
+            "</testsuite>";
+    private String defaultClassName = "com.odde.bbuddy.RequireMergeTest$SubClass";
 
     @Before
     public void createTask() throws IOException {
@@ -34,47 +44,48 @@ public class MergeUnitTestResultsTaskTest {
     }
 
     @Before
-    public void initializeFolderNames() {
-        targetTestResultsFolder = tmpFolder.getRoot() + separator + "build" + separator + "test-results";
+    public void initSourceTestResultsFolder() {
         sourceTestResultsFolder = tmpFolder.getRoot() + separator + "build" + separator + "test-results";
     }
 
     @Test
-    public void should_success_when_no_result_need_merge() throws IOException {
-        createTestResultFile("RequireNoMergeTest");
+    public void should_not_change_test_result_file_not_matching() throws IOException {
+        createTestResultFile("RequireNoChangeTest", defaultClassName);
 
         runTask();
 
         assertTaskRunSuccess();
+        assertTestResultFileContentUnchanged(defaultXml, "RequireNoChangeTest");
     }
 
     @Test
-    public void should_create_a_new_test_result_file_with_same_content_of_nested_runner_result() throws IOException {
-        createTestResultFile("RequireNoMergeTest");
-        createTestResultFile("TransactionAddControllerTest$Add");
+    public void should_create_a_new_test_result_file_with_same_content_of_nested_runner_result() throws IOException, ParserConfigurationException, SAXException {
+        createTestResultFile("RequireMergeTest$SubClass", "com.odde.bbuddy.RequireMergeTest$SubClass");
 
         runTask();
 
-        assertFileExists(true, "TransactionAddControllerTest");
+        assertTaskRunSuccess();
+        assertXmlFileAttributeChanged("com.odde.bbuddy.RequireMergeTest", "name", "RequireMergeTest$SubClass");
     }
 
-    private void assertFileExists(boolean expected, String fileName) {
-        assertThat(testResultFile(targetTestResultsFolder, fileName).exists()).isEqualTo(expected);
+    private void assertXmlFileAttributeChanged(String expected, String attributeName, String shortName) throws SAXException, IOException, ParserConfigurationException {
+        assertThat(document(shortName).getDocumentElement().getAttribute(attributeName)).isEqualTo(expected);
     }
 
-    private File testResultFile(String folder, String fileName) {
-        return new File(folder + separator + format(testResultFileNamePattern, fileName));
+    private Document document(String shortName) throws SAXException, IOException, ParserConfigurationException {
+        return newInstance().newDocumentBuilder().parse(testResultFile(shortName));
     }
 
-    private void createTestResultFile(String fileName) throws IOException {
-        String defaultXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<testsuite name=\"com.odde.bbuddy.account.controller.AccountAddControllerTest\" tests=\"1\" skipped=\"0\" failures=\"0\" errors=\"0\" timestamp=\"2016-11-06T10:32:48\" hostname=\"Josephs-MBP-2\" time=\"0.041\">\n" +
-                "  <properties/>\n" +
-                "  <testcase name=\"should_not_add_account\" classname=\"com.odde.bbuddy.account.controller.AccountAddControllerTest\" time=\"0.041\"/>\n" +
-                "  <system-out><![CDATA[]]></system-out>\n" +
-                "  <system-err><![CDATA[]]></system-err>\n" +
-                "</testsuite>";
-        writeStringToFile(testResultFile(sourceTestResultsFolder, fileName), defaultXml);
+    private void assertTestResultFileContentUnchanged(String defaultXml, String shortName) throws IOException {
+        assertThat(readFileToString(testResultFile(shortName))).isEqualTo(defaultXml);
+    }
+
+    private File testResultFile(String shortName) {
+        return new File(sourceTestResultsFolder + separator + format(testResultFileNamePattern, shortName));
+    }
+
+    private void createTestResultFile(String shortName, String className) throws IOException {
+        writeStringToFile(testResultFile(shortName), defaultXml.replace(defaultClassName, className));
     }
 
     private void assertTaskRunSuccess() {
@@ -89,11 +100,7 @@ public class MergeUnitTestResultsTaskTest {
     }
 
     private File gradleFile() {
-        return new File(workingDirectory() + separator + "build.gradle");
-    }
-
-    private String workingDirectory() {
-        return System.getProperty("user.dir");
+        return new File(System.getProperty("user.dir") + separator + "build.gradle");
     }
 
 }
