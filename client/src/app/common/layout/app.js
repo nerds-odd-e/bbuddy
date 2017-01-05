0befa926c866754cdae00101808dd08071c7062a
Jackson Zhang <zbcjackson@gmail.com>
2017-01-05 15:57:19 +0800

    Add loading progress bar

--- client/src/app/common/layout/app.js
+++ client/src/app/common/layout/app.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import 'angular-loading-bar/build/loading-bar.css'
 import '../../../sass/layout.css'
 import angular from 'angular'
+import loading from 'angular-loading-bar'
+import animate from 'angular-animate'
 import uiRouter from 'angular-ui-router'
 import header from './header'
 import sidebar from './sidebar'
@@ -26,7 +29,11 @@ function routing($stateProvider) {
 routing.$inject = ['$stateProvider']
 
 export default angular
-    .module('app', [uiRouter, header, sidebar])
+    .module('app', [uiRouter, loading, header, sidebar])
     .component('app', app)
     .config(routing)
+    .run(($transitions, cfpLoadingBar)=>{
+        $transitions.onStart({}, cfpLoadingBar.start)
+        $transitions.onSuccess({}, cfpLoadingBar.complete)
+    })
     .name
