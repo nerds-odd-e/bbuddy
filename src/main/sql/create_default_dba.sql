94f32f92d8c75b49f7620d8a4226bb47b0e0dbfe
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-05-28 22:49:38 +0800

    update the create user sql for the latest mysql version

--- src/main/sql/create_default_dba.sql
+++ src/main/sql/create_default_dba.sql
@@ -1,3 +1,3 @@
-CREATE USER 'nerd'@'localhost' IDENTIFIED BY 'dbs3cr3t';
+CREATE USER 'nerd'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dbs3cr3t';
 GRANT ALL PRIVILEGES ON *.* TO 'nerd'@'localhost';
 FLUSH PRIVILEGES;
